022d359787ddaa95cb474a205831b6c260ddbae0
Leah Ferrell <leahferrell03@gmail.com>
2018-09-25 21:22:18 -0500

    created initial layout

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import Shape from '../model/shape'
-import '../styles.css'
+import Shape, {Block} from '../model/shape'
+import './layout.css'
 
 export class Layout extends React.Component {
     render(){
         return (
            <div className="game-board">
-                <div className="main-window"/>
-                <div className="next-block-window">
-                    <Shape name="i"/>
-                    <Shape name="j"/>
-                    <Shape name="t"/>
-                    <Shape name="o"/>
-                    <Shape name="l"/>
-                    <Shape name="s"/>
-                    <Shape name="z"/>
-                </div>
-                <div className="current-level"/>
-                <div className="lines"/>
-                <div className="score"/>
+               <div className="main-window">
+                   {/*<Shape name="i"/>
+                   <Shape name="j"/>
+                   <Shape name="t"/>
+                   <Shape name="o"/>
+                   <Shape name="l"/>
+                   <Shape name="s"/>
+                   <Shape name="z"/>*/}
+                   {Layout.renderBlockGrid(10,18)}
+               </div>
+               <div className="right-window">
+                   <div className="hold-block-window">
+                       <Shape name="z"/>
+                   </div>
+                   <div className="next-block-window">
+                       <Shape name="z"/>
+                   </div>
+                   <div className="current-level"/>
+                   <div className="lines"/>
+                   <div className="score"/>
+               </div>
             </div>
         )
+    }
+    static renderBlockGrid(maxX, maxY) {
+        let blocks = [];
+        let size = maxX*maxY;
+        for (let i = 0; i < size; i++) {
+            blocks.push(<Block/>);
+        }
+        return blocks;
     }
 }
--- src/model/shape.js
+++ src/model/shape.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './shape.css';
 
 export function Block(props) {
-    return (<div className={props.shape + "-shape block"}/>)
+    let classes = (props.shape ? props.shape : "") + "block";
+    return (<div className={classes}/>)
 }
 
 class Shape extends React.Component{
     render(){
         return (
             <div className="shape">
-                <Block shape={this.props.name}/>
-                <Block shape={this.props.name}/>
-                <Block shape={this.props.name}/>
-                <Block shape={this.props.name}/>
+                <Block shape={this.props.name + "-shape "}/>
+                <Block shape={this.props.name + "-shape "}/>
+                <Block shape={this.props.name + "-shape "}/>
+                <Block shape={this.props.name + "-shape "}/>
             </div>
         )
     }
